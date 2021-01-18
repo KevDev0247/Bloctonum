bce6a2c701c489c6890c72b401fe985327b74d32
KevDev0247 <donkevin1980s@gmail.com>
2021-01-17 23:26:57 -0500

    Write into a block

--- src/block.rs
+++ src/block.rs
@@ -4,20 +4,25 @@ use super::*;
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
-    pub hash: BlockHash,
-    pub prev_block_hash: BlockHash,
+    pub hash: Hash,
+    pub prev_block_hash: Hash,
     pub nonce: u64,
     pub payload: String,
 }
 
 impl Debug for Block {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block")
+        write!(f, "Block[{}]: {} at: {} with: {}", 
+            &self.index, 
+            &self.hash, 
+            &self.timestamp, 
+            &self.payload
+        )
     }
 }
 
 impl Block {
-    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, 
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: Hash, 
         nonce: u64, payload: String) -> Self {
             Block {
                 index,
