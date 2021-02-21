6365bf31c6cd9a92548f488a64705c0f6e2078ed
KevDev0247 <donkevin1980s@gmail.com>
2021-02-20 21:31:34 -0500

    Create blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Bloconum Block".to_owned(), 0x0000ffffffffffffffffffffffffffff);
-
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Bloconum Block".to_owned(), difficulty);
 
     block.mine();
+    println!("Mined bloconum block {:?}", &block);
 
-    println!("{:?}", &block);
+    let mut previous_hash = block.hash.clone();
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+    for i in 1..=10 {
+        let mut block = Block::new(i, 0, previous_hash, 0, "New Bloconum Block".to_owned(), difficulty);
+
+        block.mine();
+        println!("Mined bloconum block {:?}", &block);
+
+        previous_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+    }
 }
